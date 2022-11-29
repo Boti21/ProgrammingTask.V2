db6042a1793540c1dab4116958f332d3b3dc2dfa
Boti21 <botibencsik@gmail.com>
2022-11-29 23:17:43 +0100

    major modification and optimization mostly

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -17,41 +18,31 @@
 #include "usart.h"
 
 
-volatile unsigned char dutyCycle = 0; //value = 0-255; duty cycle formula: dutyCycle/255=percentage, has to be declared outside main for the ISR to access it
-volatile float dutyHelper = 0;
+volatile float dutyCycle = 0; //value = 0-255; duty cycle formula: dutyCycle/255=percentage, has to be declared outside main for the ISR to access it
+volatile bool motor_run = 0;
 
-unsigned char motor_run = 0;
 
-volatile char rx_end = 0;
-volatile char rx_in;
+volatile bool rx_end = 0;
+volatile unsigned char rx_in;
 volatile unsigned char rx_strike = 0;
-volatile unsigned char rx_error_strike = 0;
-volatile unsigned char rx_val = 0;
+volatile bool rx_val = 0;
 volatile unsigned char rx_page = 0;
-volatile unsigned char rx_error = 0;
 
 volatile unsigned int time_temp = 0;
-volatile double time = 0.0;
-volatile unsigned char overflow_flag = 0;
-volatile double timer = 0.0;
+volatile float time = 0.0;
+volatile bool overflow_flag = 0;
+volatile unsigned char overflow_count = 0;
+
+volatile float timer = 0.0;
+volatile float timer2 = 0.0;
+volatile bool timer_running = 0;
 
 ISR(USART_RX_vect)
 {
     rx_in = UDR0;
-    if(rx_in == 0x71 && rx_val == 0 && rx_error == 0) //First bit; incoming datatype flag
+    if(rx_in == 0x71 && rx_val == 0) //First bit; incoming datatype flag
         rx_val = 1;
-    else if(rx_in == 0x1A && rx_val == 0 && rx_error == 0)
-        rx_error = 1;
-    else if(rx_in == 0xFF && rx_error == 1)
-    {
-        rx_error_strike++;
-        if(rx_error_strike == 3)
-        {
-            rx_error = 0;
-            rx_error_strike = 0;
-        }
-    }
-    else if(rx_in == 0xFF && rx_val == 1 && rx_error == 0)
+    else if(rx_in == 0xFF && rx_val == 1)
     {
         rx_strike++;
         if(rx_strike == 3)
@@ -60,32 +51,27 @@ ISR(USART_RX_vect)
             rx_strike = 0;
         }
     }
-    else if(rx_val == 1 && rx_error == 0 && rx_in <= 100)
+    else if(rx_val == 1 && rx_in <= 100)
     {
         dutyCycle = 255 * (rx_in/100.0);
-        OCR0A = dutyCycle;
-        OCR0B = OCR0A;
+        OCR0A = (int)dutyCycle;
     }
-    if(rx_val == 1 && rx_error == 0)
+    if(rx_val == 1)
     {
         switch (rx_in)
         {
             case 101: //increase duty cycle by 10%
                 if((dutyCycle + 25) <= 255)
                 {
-                    dutyHelper = dutyHelper + 25.5;
-                    dutyCycle = dutyHelper;
-                    OCR0A = dutyCycle;
-                    OCR0B = OCR0A;
+                    dutyCycle = dutyCycle + 25.5;
+                    OCR0A = (int)dutyCycle;
                 }
                 break;
             case 102: //decrease duty cycle by 10%
                 if((dutyCycle - 25) >= 0)
                 {
-                    dutyHelper = dutyHelper - 25.5;
-                    dutyCycle = dutyHelper;
-                    OCR0A = dutyCycle;
-                    OCR0B = OCR0A;
+                    dutyCycle = dutyCycle - 25.5;
+                    OCR0A = (int)dutyCycle;
                 }
                 break;
             case 103: //start motor
@@ -106,13 +92,21 @@ ISR(USART_RX_vect)
     }
 }
 
+ISR(TIMER0_OVF_vect)
+{
+    if(timer_running == 1)
+    {
+        timer += 255.0 / 15625.0;
+    }
+}
+
 
 ISR(TIMER1_OVF_vect) //Timer/Counter1 Overflow Interrupt to keep track of time even when the TCNT1 register reset without a detection
 {
 
     if(overflow_flag == 1)
     {
-        time_temp += 65535;
+        overflow_count++;
     }
     overflow_flag = 1;
 
@@ -154,8 +148,8 @@ int main(void) {
     TCCR0A |= 0b00000011; //original value 0b10100011
     //TCCR0A &= ~((1 << COM0A1) | (1 << COM0B1));
     TCCR0B |= (0 << WGM02 ) |(1 << CS02) | (0 << CS01) | (1 << CS00);
-    OCR0A |= dutyCycle; //When the compare match occurs OC0A is cleared (output pins cleared)
-    OCR0B = OCR0A;
+    //OCR0A |= dutyCycle; //When the compare match occurs OC0A is cleared (output pins cleared)
+    TIMSK0 |= 0x01; //enable overflow interrupt
 
     //ADC
     ADMUX = 0b01000000; //ADC0 selected, PC0, A0 on arduino
@@ -168,7 +162,7 @@ int main(void) {
     UCSR0A |= (1 << RXC0);
     UCSR0B |= (1 << RXCIE0);
     //UDR0; //recieved data will be in this register
-    PORTD |= 0x03;
+    PORTD |= 0x03; //Enable pullup on PD0-1
     
     //printf("%c%c%c", 255, 255, 255);
 
@@ -189,14 +183,20 @@ int main(void) {
                     if(motor_run == 0)
                     {   
                         TCCR0A &= ~((1 << COM0A1) | (1 << COM0B1));
-                        //printf("TCCR0A=%d%c%c%c", TCCR0A, 255, 255, 255); //Debug command
+                        timer_running = 0;
+                        timer = 0;
                     }
                     else if(motor_run == 1)
                     {
                         TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
-                        //printf("TCCR0A=%d%c%c%c", TCCR0A, 255, 255, 255); //Debug command
+                        timer_running = 1;
                     }
                     rx_end = 0;
+                }
+
+                if(timer_running == 1)
+                {
+                    printf("timeval.val=%d%c%c%c", (int)(timer*10), 255, 255, 255);
                 }
             }
 
@@ -204,22 +204,20 @@ int main(void) {
         while(TIFR1 & (1<<ICF1))
         {
 
-            time = ((ICR1 + time_temp) / (16000000.0/1024.0));
+            time = ((ICR1 + (overflow_count * 65535)) / (16000000.0/1024.0));
             TCNT1 = 0; //Clock reset
             TIFR1 = TIFR1 | 0b00100000; //TIFR1 = (1<<ICF1); these are doing the same thing: reset the input capture flag
-            time_temp = 0;
             overflow_flag = 0;
             
             speed = DIST_CONST / time;
             dist = dist + DIST_CONST;
-            timer += time;
 
             if(rx_page == 0)
             {
                 //Display instructions, occasional multiplication to adjust the values for the screen to be displayed properly
                 printf("speedval.val=%d%c%c%c", (int)speed, 255, 255, 255);
                 printf("distval.val=%d%c%c%c", (int)dist, 255 , 255, 255);
-                printf("timeval.val=%d%c%c%c", (int)(timer * 10), 255, 255, 255);
+                //printf("timeval.val=%d%c%c%c", (int)(timer * 10), 255, 255, 255);
             }
 
         }
